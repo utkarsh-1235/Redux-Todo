6047686c6078ee3e480909bfeab0bc560add158c
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-12-22 23:53:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./Components/AddTodo"
+import Todo from "./Components/Todo"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-2xl font-bold text-center">Hello Utkarsh</h1>
+    <AddTodo/>
+    <Todo/>
+    </>
+  )
+}
+
+export default App
